4b67d06b2acb4b48afb2753924d9aaa84abbe46a
Giang Nguyen <ngttgiang@gmail.com>
2020-09-10 19:14:30 +0200

    Add files via upload
    version 2 with enhancements

--- ui.R
+++ ui.R
@@ -1,134 +1,137 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Make a Portfolio"),
-
-    
-
-    # Sidebar 
-    sidebarLayout(
-        sidebarPanel(
-
-            # text input for sector1
-            # textInput(inputId = "sector1",
-            #           label = "Choose sector",
-            #           value = "Finance",
-            #           placeholder = 'Enter a sector'
-            # ),
-            
-            #select sector
-            selectInput(inputId = 'sector1',
-                        label = 'Select sector',
-                        choices = c('Finance'= 'Finance', 
-                                    'Technology'='Technology', 
-                                    'Healthcare'='Healthcare'),
-                        multiple = FALSE,
-                        selected = 'Finance'
-            ),
-            
-            # text input for sector2
-         
-            
-            # text input for s_date
-            textInput(inputId = "sdate",
-                      label = "Start date for lookback period",
-                      value = "2018-01-01",
-                      placeholder = 'Enter a date yyyy-mm-dd'
-            ),
-            
-           
-            # input number of month
-            numericInput(inputId = "nmonth",
-                      label = "Number of month from the lookback start date",
-                      value = 12,
-                      min = 1,
-                      max = 12,
-                      step = 1
-            ),
-            
-            # input number of stock to pick
-            numericInput(inputId = "N",
-                         label = "Number of stock to pick",
-                         value = 2,
-                         min = 1,
-                         max = 5,
-                         step = 1
-            ),
-            
-            # input number of trading volume
-            numericInput(inputId = "vol",
-                         label = "Threshold of trading volume",
-                         value = 1e6
-            ),
-            
-            # input number of market cap
-            numericInput(inputId = "cap",
-                         label = "Threshold of market cap",
-                         value = 1e9
-            ),
-            
-           actionButton(
-               inputId = 'submit_loc',
-               label = "Submit"
+  
+  #Application title
+  hr(),
+  
+  titlePanel("Customize your portfolio:"),
+  
+  fluidRow(
+    column(5,
+           h4("Lookback period"),
+           #select sector
+           selectInput(inputId = 'sector1',
+                       label = 'Sector',
+                       choices = c('Finance'= 'Finance', 
+                                   'Technology'='Technology', 
+                                   'Health Care'='Health Care',
+                                   'Energy' = 'Energy'),
+                       multiple = FALSE,
+                       selected = 'Technology'
            ),
-            
-            # text input for s_date portfolio
-            textInput(inputId = "p_sdate",
-                      label = "Start date for portfolio",
-                      value = "2018-01-01",
-                      placeholder = 'Enter a date yyyy-mm-dd'
-            ),
-            
-            # input number of month
-            numericInput(inputId = "p_nmonth",
-                         label = "Number of month from the portfolio start date",
-                         value = 12,
-                         min = 1,
-                         max = 12,
-                         step = 1
-            ),
-            
-            # input number of rebalance frequency
-            numericInput(inputId = "rebalance",
-                         label = "Rebalance frequency",
-                         value = 3,
-                         min = 1,
-                         max = 12,
-                         step = 1
-            ),
-            
-            # input number of initial quantity
-            numericInput(inputId = "init_quantity",
-                         label = "Initial Quantity",
-                         value = 10
-            ),
-            
-            # Risk free rate
-            numericInput(inputId = "rf",
-                         label = "Risk-free rate",
-                         value = 0.06
-            ),
-        ),
-            
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            tableOutput("stocks"),
-            
-            plotOutput("portfolioPlot")
-        )
-    )
-))
+           
+           # input number of trading volume
+           numericInput(inputId = "vol",
+                        label = "Threshold of trading volume",
+                        value = 1e6
+           ),
+           
+           # input number of market cap
+           numericInput(inputId = "cap",
+                        label = "Threshold of market cap(USD)",
+                        value = 1e11
+           ),
+           
+           
+           # text input for s_date
+           textInput(inputId = "sdate",
+                     label = "Start date",
+                     value = "2017-01-01",
+                     placeholder = 'Enter a date yyyy-mm-dd'
+           ),
+           
+           
+           # input number of month
+           numericInput(inputId = "nmonth",
+                        label = "Number of months from lookback start date ",
+                        value = 12,
+                        min = 1,
+                        max = 12,
+                        step = 1
+           ),
+           
+           # input number of stock to pick
+           numericInput(inputId = "N",
+                        label = "Number of stocks to pick",
+                        value = 2,
+                        min = 1,
+                        max = 5,
+                        step = 1
+           ),
+           
+           
+           actionButton(
+             inputId = 'submit_loc',
+             label = "Submit"
+           )
+           
+    ),
+    column(5, h4("Investment period"), offset = 1,
+           # text input for s_date portfolio
+           textInput(inputId = "p_sdate",
+                     label = "Start date",
+                     value = "2018-01-01",
+                     placeholder = 'Enter a date yyyy-mm-dd'
+           ),
+           
+           # input number of month
+           numericInput(inputId = "p_nmonth",
+                        label = "Number of holding months",
+                        value = 12,
+                        min = 1,
+                        max = 12,
+                        step = 1
+           ),
+           
+           # input number of rebalance frequency
+           numericInput(inputId = "rebalance",
+                        label = "Rebalance frequency",
+                        value = 3,
+                        min = 1,
+                        max = 12,
+                        step = 1
+           ),
+           
+           # input number of initial quantity
+           numericInput(inputId = "init_quantity",
+                        label = "Initial invested quantity",
+                        value = 10
+           ),
+           
+           # Risk free rate
+           numericInput(inputId = "rf",
+                        label = "Risk-free rate",
+                        value = 0.06
+           ),
+    ),
+       
+  ),
+  
+  hr(),
+  
+  
+  mainPanel(
+    textOutput('state'),
+    hr(),
+    textOutput('noStock'),
+    # textOutput('numstock'),
+    
+    conditionalPanel( condition = "output.state =='Investment Portfolio Details'",
+                     conditionalPanel(condition = "output.noStock != 'There are no stocks meet the provided criteria'",
+                                      #selected stocks
+                                      tableOutput("stock_table"),
+                                      
+                                      # Portfolio performance ratio
+                                      tableOutput('portfolio_ratios'),
+                                      
+                                      #plot of cumulative PnL
+                                      plotOutput('plot')
+                                      )
+                      )
+    
+    )#end main panel
+  )#fluidPage
+)#end UI
 
